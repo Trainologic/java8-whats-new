094d3f57aebad94b8a5343fd87bfc90a32f31d8c
Victor Perepelitsky <victor.prp@gmail.com>
2017-01-12 17:05:44 +0200

    java8

--- exercises/src/main/java/Ex4.java
+++ exercises/src/main/java/Ex4.java
@@ -4,7 +4,6 @@
 public class Ex4 {
 
 
-
     /**
      * @return the number of Fridays the 13th since 1/1/1970 till now.
      */
@@ -13,10 +12,7 @@ public class Ex4 {
     }
 
 
-
-
     public static void main(String... args){
         System.out.println("V1 - The number of Fridays 13 are:" + fridays13());
-
     }
 }
