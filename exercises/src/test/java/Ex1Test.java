3d73994ed763f240b1f27f5171fea448e3a0ccfe
Victor Perepelitsky <victor.prp@gmail.com>
2017-01-12 13:05:46 +0200

    java8

--- exercises/src/test/java/Ex1Test.java
+++ exercises/src/test/java/Ex1Test.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -20,6 +23,27 @@ public class Ex1Test {
         BiFunction<String,String,String> f = (a,b) -> a+b;
         Assert.assertEquals("abcdef", f.apply("abc","def"));
         Assert.assertEquals("defabc", Ex1.flip(f).apply("abc", "def"));
+    }
+
+
+
+    @Test
+    public void transformableTest() {
+        Ex1.TransformableStringSet words = new  Ex1.TransformableStringSet(Arrays.asList("hello", "java8"));
+        Set<String> expected = new HashSet<>(Arrays.asList("HELLO","JAVA8"));
+
+        Assert.assertEquals(expected,words.transform(String::toUpperCase));
+
+
+    }
+
+    @Test
+    public void capitalizeTest() {
+
+        Set<String> words = new HashSet<>(Arrays.asList("hello","java8"));
+        Set<String> expected = new HashSet<>(Arrays.asList("Hello","Java8"));
+
+        Assert.assertEquals(expected,Ex1.capitalize(words));
     }
 
     @Test
