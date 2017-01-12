482f2ea2307340c84611acc7af7a8a5628226c16
Victor Perepelitsky <victor.prp@gmail.com>
2017-01-12 12:58:33 +0200

    java8

--- exercises/src/main/java/Ex1.java
+++ exercises/src/main/java/Ex1.java
@@ -1,3 +1,6 @@
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -18,16 +21,57 @@ public class Ex1 {
      * @param f a function that takes two arguments and returns one
      * @return a function that does exactly what f does but flips the input arguments
      *
-     * <p>
-     * Example:<p>
-     * BiFunction<Int,Int,Int> f = (a,b) -> a-b <p>
-     * f.apply(3,2) //the result is 1 <p>
-     * flip(f).apply(3,2) //the result is -1 <p>
-     *
      */
     public static <A,B,C> BiFunction<B,A,C> flip(BiFunction<A,B,C> f) {
         return null;
     }
+
+
+    /**
+     * Impl transform function
+     */
+    public static class TransformableStringSet extends HashSet<String>{
+        public TransformableStringSet() {
+        }
+
+        public TransformableStringSet(Collection<? extends String> c) {
+            super(c);
+        }
+
+        TransformableStringSet transform(Function<String,String> transformer ){
+            TransformableStringSet result = new TransformableStringSet();
+            for (String element: this) {
+                result.add(transformer.apply(element));
+            }
+            return result;
+        }
+    }
+
+
+    /**
+     *
+     * @return  the given word with Capital first letter.
+     *          if the word is empty returns empty string.
+     */
+    public static String capitalize(String word){
+        if (word.length() <1) return word;
+
+        char[] result = word.toCharArray();
+        result[0] = Character.toUpperCase(word.toCharArray()[0]);
+        return new String(result);
+    }
+
+
+    /**
+     * Hint: use {@link TransformableStringSet}
+     * @return the given words after capitalization
+     */
+    public static Set<String> capitalize(Set<String> words){
+        TransformableStringSet result = new TransformableStringSet(words);
+        return result.transform(Ex1::capitalize);
+    }
+
+
 
 
     /**
--- exercises/src/test/java/Ex1Test.java
+++ exercises/src/test/java/Ex1Test.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -22,7 +25,28 @@ public class Ex1Test {
         Assert.assertEquals("defabc", Ex1.flip(f).apply("abc", "def"));
     }
 
+
     @Test
+    public void transformableTest() {
+        Ex1.TransformableStringSet words = new  Ex1.TransformableStringSet(Arrays.asList("hello","java8"));
+        Set<String> expected = new HashSet<>(Arrays.asList("HELLO","JAVA8"));
+
+        Assert.assertEquals(expected,words.transform(String::toUpperCase));
+
+
+    }
+
+    @Test
+    public void capitalizeTest() {
+
+        Set<String> words = new HashSet<>(Arrays.asList("hello","java8"));
+        Set<String> expected = new HashSet<>(Arrays.asList("Hello","Java8"));
+
+        Assert.assertEquals(expected,Ex1.capitalize(words));
+    }
+
+
+        @Test
     public void cur(){
         BiFunction<String,String,String> f = (a,b) -> a+b;
         Function<String,String> f1 = Ex1.cur(f).apply("abc");
